cf5bbafcf76e33594483c02cbbd65cccf5cf8b62
Jorys Paulin <jorys@jorys-paulin.fr>
2023-11-30 14:02:07 +0100

    feat: initial commands

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,107 @@
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
+ * Moonlight Discord
  */
 
+import { InteractionResponseType, InteractionType, verifyKey } from 'discord-interactions';
+
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	// Discord application ID
+	DISCORD_APPLICATION_ID: string;
+
+	// Discord public key
+	DISCORD_PUBLIC_KEY: string;
+
+	// Discord token
+	DISCORD_TOKEN: string;
+}
+
+// Returns a JSON reponse
+function createJsonResponse(value: any) {
+	return new Response(JSON.stringify(value), { headers: { 'Content-Type': 'application/json;charset=UTF-8' } });
+}
+
+// Returns an error as JSON
+function errorResponse(error: string) {
+	return new Response(JSON.stringify({ error }), { status: 400, headers: { 'Content-Type': 'application/json;charset=UTF-8' } });
+}
+
+// Returns a Discord message
+function messageResponse(message: { content: string }) {
+	return new Response(JSON.stringify({ type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE, data: message }), {
+		headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+	});
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+
+		// Index page
+		if (request.method === 'GET' && url.pathname === '/') {
+			return Response.redirect('https://moonlight-stream.org/discord', 302);
+		}
+
+		// Interactions route
+		if (request.method === 'POST' && url.pathname === '/') {
+			const signature = request.headers.get('X-Signature-Ed25519');
+			const timestamp = request.headers.get('X-Signature-Timestamp');
+			const body = await request.text();
+
+			// Check signature
+			if (!signature || !timestamp || !verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY)) {
+				return new Response('Baq Request Signature', { status: 401 });
+			}
+
+			const interaction = JSON.parse(body);
+
+			// Ping
+			if (interaction.type === InteractionType.PING) {
+				return createJsonResponse({ type: InteractionResponseType.PONG });
+			}
+
+			// Application commands
+			if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+				const commandName = interaction.data.name;
+
+				// Wiki
+				if (commandName === 'wiki') {
+					return messageResponse({ content: 'https://github.com/moonlight-stream/moonlight-docs/wiki' });
+				}
+				// Setup guide
+				if (commandName === 'setup') {
+					return messageResponse({ content: 'https://github.com/moonlight-stream/moonlight-docs/wiki/Setup-Guide' });
+				}
+				// Faq
+				if (commandName === 'faq') {
+					return messageResponse({ content: 'https://github.com/moonlight-stream/moonlight-docs/wiki/Frequently-Asked-Questions' });
+				}
+				// Gamepad tester
+				if (commandName === 'gamepadtester') {
+					return messageResponse({ content: 'https://gamepad-tester.com/' });
+				}
+				// Shortcuts
+				if (commandName === 'shortcuts') {
+					return messageResponse({
+						content: 'https://github.com/moonlight-stream/moonlight-docs/wiki/Setup-Guide#keyboardmousegamepad-input-options',
+					});
+				}
+
+				// Duplicate message
+				if (commandName === 'Duplicate message') {
+					return messageResponse({ content: "Please don't post your message in multiple channels" });
+				}
+				// Cheese
+				if (commandName === 'Cheese') {
+					return messageResponse({ content: ':cheese:' });
+				}
+
+				return errorResponse('Unknown Command');
+			}
+
+			return errorResponse('Unknown Type');
+		}
+
+		// Default route
+		return new Response('Not Found', { status: 404 });
 	},
 };
